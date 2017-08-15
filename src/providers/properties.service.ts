61dcaa5f056957200251591630dfb74530bc34a6
Icaro F Guselian <icaro.ifg@gmail.com>
2017-08-15 20:08:06 -0300

    Adicionando fluxo de login e cadastro

---
+++ src/providers/properties.service.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Vladimir Budilov on 8/13/16.
+ */
+
+export let _REGION = "us-east-1";
+
+export let _IDENTITY_POOL_ID = "us-east-1:a6c80bf4-963a-4d43-b8b8-4b136bae82f5";
+export let _USER_POOL_ID = "us-east-1_umDo8TdRv";
+export let _CLIENT_ID = "2gt068nd4oggb6h7ufucklnvj9";
+export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
+
+export let _POOL_DATA = {
+    UserPoolId: _USER_POOL_ID,
+    ClientId: _CLIENT_ID
+};
