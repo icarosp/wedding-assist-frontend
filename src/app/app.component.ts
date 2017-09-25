b7d0d4e99dbda30b9de77a28aabd9cffea6a20c6
Icaro F. Guselian <icaro.ifg@gmail.com>
2017-09-25 18:32:48 -0300

    adding android platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class MyApp {
       this.awsUtil.initAwsService();
   
       console.log("Set login component as root");
-      this.rootPage = TabsPage;
+      this.rootPage = LoginComponent;
   
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
